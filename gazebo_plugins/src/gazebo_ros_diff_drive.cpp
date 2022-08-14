de73348fd5fe4e3e96734cac7b4d21e91414ac49
Jonathan Sanabria <45742788+1hada@users.noreply.github.com>
2022-08-14 00:53:22 -0400

    update to not change joint state directly

--- gazebo_plugins/src/gazebo_ros_diff_drive.cpp
+++ gazebo_plugins/src/gazebo_ros_diff_drive.cpp
@@ -469,28 +469,21 @@ void GazeboRosDiffDrive::UpdateOdometryEncoder()
         is_delayed_start_ = false;
     }
 
-    double vl, vr;
+    double vl = joints_[LEFT]->GetVelocity ( 0 );
+    double vr = joints_[RIGHT]->GetVelocity ( 0 );
     if(did_not_read)
     {
         // No encoder update. The hardware interface board is still sending messages, but the encoders are not
         //  updating. Results in a 0 speed signal.
         if(left_wheel_dropped_ || is_delayed_start_)
         {
-            double simulated_noise = GaussianKernel(noise_at_dropout_mu_, noise_at_dropout_sigma_);
-            // sets the joint to that value such that the diff drive controller can then update it after the dropout
-            // better simulates the system during dropouts, although the wheel's speed is actually increased in the true dropouts scenario
-            joints_[LEFT]->SetParam ( "vel", 0, simulated_noise );
+            vl = GaussianKernel(noise_at_dropout_mu_, noise_at_dropout_sigma_);
         }
         if(right_wheel_dropped_ || is_delayed_start_)
         {
-            double simulated_noise = GaussianKernel(noise_at_dropout_mu_, noise_at_dropout_sigma_);
-            // sets the joint to that value such that the diff drive controller can then update it after the dropout
-            // better simulates the system during dropouts, although the wheel's speed is actually increased in the true dropouts scenario
-            joints_[RIGHT]->SetParam ( "vel", 0, simulated_noise );
+            vr = GaussianKernel(noise_at_dropout_mu_, noise_at_dropout_sigma_);
         }
     }
-    vl = joints_[LEFT]->GetVelocity ( 0 );
-    vr = joints_[RIGHT]->GetVelocity ( 0 );
 
     double seconds_since_last_update = ( current_time - last_odom_update_ ).Double();
     last_odom_update_ = current_time;
